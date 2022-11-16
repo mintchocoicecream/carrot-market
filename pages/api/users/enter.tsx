5d16c4f0109cb1b34da0151cfc4b0ef429c5360c
mintchocoicecream <sxxhnjng@gmail.com>
2022-11-16 16:01:43 +0900

    #Authentication 01
    01. Accounts Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -4,8 +4,56 @@ import withHandler from "@libs/server/withHandler";
 
 
 
-function handler(req: NextApiRequest,res: NextApiResponse) {
-    console.log(req.body);
+async function handler(req: NextApiRequest,res: NextApiResponse) {
+    const {phone , email} = req.body;
+    const payload = phone?{phone: +phone}:{email};
+    const user = await client.user.upsert({
+        where: {
+            ...payload,
+        },
+        create: {
+            name: "Anonymous",
+            ...payload,
+        },
+        update: {},
+    });
+    console.log(user);
+/*     if(phone){
+        user = await client.user.findUnique({
+            where: {
+                phone: +phone,
+            },
+        });
+        if(user) console.log("found it!");
+        if(!user){
+            console.log("not find it:( Will create");
+            user = await client.user.create({
+                data: {
+                    name: "Anonymous",
+                    phone: +phone,
+                }
+            });
+        }
+        console.log(user);
+    }
+    if(email){
+        user = await client.user.findUnique({
+            where: {
+                email,
+            },
+        });
+        if(user) console.log("found it!");
+        if(!user){
+            console.log("not find it:( Will create");
+            user = await client.user.create({
+                data: {
+                    name: "Anonymous",
+                    email,
+                }
+            });
+        }
+        console.log(user);
+    } */
     return res.status(200).end();
 }
 
