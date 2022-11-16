49bb013c6cd326e4e1aa3203e2ffa4e518943706
mintchocoicecream <sxxhnjng@gmail.com>
2022-11-16 21:18:05 +0900

    #Authentication 05
    05. Token UI

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -11,9 +11,19 @@ interface EnterForm {
   phone?: string;
 }
 
+interface TokenForm {
+  token: string;
+}
+
+interface MutationResult {
+  ok:boolean;
+}
+
 const Enter: NextPage = () => {
-  const [enter, {loading, data, error}] = useMutation("/api/users/enter");
+  const [enter, {loading, data, error}] = useMutation<MutationResult>("/api/users/enter");
+  const [confirmToken, {loading:tokenLoading, data:tokenData}] = useMutation<MutationResult>("/api/users/confirm");
   const {register, handleSubmit, reset} = useForm<EnterForm>();
+  const {register:tokenRegister, handleSubmit: tokenHandleSubmit} = useForm<TokenForm>();
   const [method, setMethod] = useState<"email" | "phone">("email");
   const onEmailClick = () => {
     reset(); 
@@ -27,11 +37,29 @@ const Enter: NextPage = () => {
     if(loading) return;
     enter(validForm);
   };
+  const onTokenValid = (validForm:TokenForm) => {
+    if(tokenLoading)return;
+    confirmToken(validForm);
+  }
   return (
     <div className="mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
       <div className="mt-12">
-        <div className="flex flex-col items-center">
+          {data?.ok ? 
+          <form onSubmit={tokenHandleSubmit(onTokenValid)} className="flex flex-col mt-8 space-y-4">
+              <Input
+                register={tokenRegister("token", {
+                  required: true,
+                })}
+                name="token"
+                label="Confirmation Token"
+                type="number"
+                required
+              />
+              <Button text={tokenLoading? "Loading" : "Confirm Token"} />
+            </form> : (
+          <>
+            <div className="flex flex-col items-center">
           <h5 className="text-sm text-gray-500 font-medium">Enter using:</h5>
           <div className="grid  border-b  w-full mt-8 grid-cols-2 ">
             <button
@@ -57,8 +85,8 @@ const Enter: NextPage = () => {
               Phone
             </button>
           </div>
-        </div>
-        <form onSubmit={handleSubmit(onValid)} className="flex flex-col mt-8 space-y-4">
+            </div>
+            <form onSubmit={handleSubmit(onValid)} className="flex flex-col mt-8 space-y-4">
           {method === "email" ? (
             <Input 
               register={register("email", {
@@ -85,7 +113,9 @@ const Enter: NextPage = () => {
           {method === "phone" ? (
             <Button text={loading? "Loading" : "Get one-time password"} />
           ) : null}
-        </form>
+            </form>
+          </>
+        )}
 
         <div className="mt-8">
           <div className="relative">
